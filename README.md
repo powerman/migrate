a92fc82ca95424abf043bf3ed9dc899cfd5a1e3d
Alex Efros <powerman@powerman.name>
2015-03-29 22:12:13 +0300

    v0.1.4

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ App::migrate - upgrade / downgrade project
 
 # VERSION
 
-This document describes App::migrate version v0.1.3
+This document describes App::migrate version v0.1.4
 
 # SYNOPSIS
 
--- lib/App/migrate.pm
+++ lib/App/migrate.pm
@@ -6,7 +6,7 @@ use utf8;
 use Carp;
 ## no critic (RequireCarping)
 
-our $VERSION = 'v0.1.3';
+our $VERSION = 'v0.1.4';
 
 use List::Util qw( first any );
 use File::Temp qw( tempfile ); # don't use Path::Tiny to have temp files in error $SHELL
@@ -453,7 +453,7 @@ App::migrate - upgrade / downgrade project
 
 =head1 VERSION
 
-This document describes App::migrate version v0.1.3
+This document describes App::migrate version v0.1.4
 
 
 =head1 SYNOPSIS
